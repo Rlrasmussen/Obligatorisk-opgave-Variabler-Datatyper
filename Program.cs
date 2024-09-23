9fe23cd45005b24334f104fd0f59660e001ad22d
Rlrasmussen <soede_rikke@hotmail.com>
2024-09-23 15:46:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_opgave___Variabler___Datatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basisprogrammering – Variabler & Datatyper");
+
+            //Hello and players name
+            Console.WriteLine("Hello, what is your name");
+            //Telling the player the mening of the game
+            Console.WriteLine("Hello " + Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
